358b3d3047d8a2be9295a8fd522fcfc5e6bff65e
vipz3 <vipz3@mail.ru>
2018-10-07 10:30:10 +0300

    ShowCase component is added

--- src/ShowCase.js
+++ src/ShowCase.js
@@ -4,7 +4,36 @@ const ShowCase = props => {
   return (
     <div>
       <React.Fragment>
-        {props.goods.length ? <div>Yes</div> : null}
+        {props.goods.length ? (
+          <div>
+            {props.goods.map((good, index) => {
+              const categoryName = Object.keys(good.category)[0];
+
+              const attributes = [];
+              for (let key in good.category[categoryName]) {
+                attributes.push(
+                  <p>
+                    {key}: {good.category[categoryName][key]}
+                  </p>
+                );
+              }
+
+              const categoryNameContainer = <h2>Category: {categoryName}</h2>,
+                goodName = <h1>Good name: {good.goodName}</h1>,
+                dateCreation = <p>Creation date: {good.dateCreation}</p>;
+
+              return (
+                <React.Fragment>
+                  {categoryNameContainer}
+                  {goodName}
+                  {dateCreation}
+                  Attributes:
+                  {attributes}
+                </React.Fragment>
+              );
+            })}
+          </div>
+        ) : null}
       </React.Fragment>
     </div>
   );
